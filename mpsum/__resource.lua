619afc8e3abdf1306a0f91e8f9ab48f1b57b362d
francescoliguori <41292339+francescoliguori@users.noreply.github.com>
2020-08-11 17:29:03 +0200

    uploaded cars file

---
+++ mpsum/__resource.lua
@@ -0,0 +1,67 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+
+ files {
+  'data/carcols.meta',
+  'data/carvariations.meta',
+  'data/clip_sets.xml',
+  'data/handling.meta',
+  'data/vehiclelayouts.meta',
+  'data/vehicles.meta',
+  'audio/dlcsum20_amp.dat10',
+  'audio/dlcsum20_amp.dat10.nametable',
+  'audio/dlcsum20_amp.dat10.rel',
+  'audio/dlcsum20_game.dat151',
+  'audio/dlcsum20_game.dat151.nametable',
+  'audio/dlcsum20_game.dat151.rel',
+  'audio/dlcsum20_sounds.dat54',
+  'audio/dlcsum20_sounds.dat54.nametable',
+  'audio/dlcsum20_sounds.dat54.rel',
+  'audio/sfx/dlc_sum20/caracara2.awc',
+ 'audio/sfx/dlc_sum20/caracara2_npc.awc',
+  'audio/sfx/dlc_sum20/club.awc',
+ 'audio/sfx/dlc_sum20/club_npc.awc',
+  'audio/sfx/dlc_sum20/coquette4.awc',
+ 'audio/sfx/dlc_sum20/coquette4_npc.awc',
+  'audio/sfx/dlc_sum20/dukes3.awc',
+ 'audio/sfx/dlc_sum20/dukes3_npc.awc',
+  'audio/sfx/dlc_sum20/gauntlet5.awc',
+ 'audio/sfx/dlc_sum20/gauntlet5_npc.awc',
+  'audio/sfx/dlc_sum20/glendale2.awc',
+ 'audio/sfx/dlc_sum20/glendale2_npc.awc',
+  'audio/sfx/dlc_sum20/landstalker2.awc',
+ 'audio/sfx/dlc_sum20/landstalker2_npc.awc',
+  'audio/sfx/dlc_sum20/manana2.awc',
+ 'audio/sfx/dlc_sum20/manana2_npc.awc',
+  'audio/sfx/dlc_sum20/openwheel1.awc',
+ 'audio/sfx/dlc_sum20/openwheel1_npc.awc',
+  'audio/sfx/dlc_sum20/openwheel2.awc',
+ 'audio/sfx/dlc_sum20/openwheel2_npc.awc',
+  'audio/sfx/dlc_sum20/penumbra2.awc',
+ 'audio/sfx/dlc_sum20/penumbra2_npc.awc',
+  'audio/sfx/dlc_sum20/peyote3.awc',
+ 'audio/sfx/dlc_sum20/peyote3_npc.awc',
+  'audio/sfx/dlc_sum20/seminole2.awc',
+ 'audio/sfx/dlc_sum20/seminole2_npc.awc',
+  'audio/sfx/dlc_sum20/tigon.awc',
+ 'audio/sfx/dlc_sum20/tigon_npc.awc',
+   'audio/sfx/dlc_sum20/yosemite3.awc',
+ 'audio/sfx/dlc_sum20/yosemite3_npc.awc',
+   'audio/sfx/dlc_sum20/youga3.awc',
+ 'audio/sfx/dlc_sum20/youga3_npc.awc',
+ 
+ 
+}
+data_file 'AUDIO_GAMEDATA' 'audio/dlcsum20_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/dlcsum20_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'audio/dlcsum20_amp.dat'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'CLIP_SETS_FILE' 'data/clip_sets.xml'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_sum20'
+
+
+client_script 'names.lua'
